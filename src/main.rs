1142d8148491ce38aa316cffbde905463f4bbc7b
cdxker <fdenzell@gmail.com>
2024-12-11 12:29:45 -0800

    feature: adjusted it to use the new api interface

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,9 @@
-use hashbrown::HashMap;
-use pagefind::{Fossicker, SearchState};
-
-pub fn new_index(config: pagefind::PagefindInboundConfig) -> Option<SearchState> {
-    match pagefind::SearchOptions::load(config) {
-        Ok(opts) => Some(SearchState::new(opts)),
-        Err(_) => None,
-    }
-}
-
-pub async fn add_record(
-    index: &mut SearchState,
-    url: String,
-    content: String,
-    language: String,
-    meta: Option<HashMap<String, String>>,
-    filters: Option<HashMap<String, Vec<String>>>,
-    sort: Option<HashMap<String, String>>,
-) -> Result<pagefind::FossickedData, ()> {
-    let data = pagefind::fossick::parser::DomParserResult {
-        digest: content,
-        filters: filters.unwrap_or_default(),
-        sort: sort.unwrap_or_default(),
-        meta: meta.unwrap_or_default(),
-        anchor_content: HashMap::new(),
-        has_custom_body: false,
-        force_inclusion: true,
-        has_html_element: true,
-        has_old_bundle_reference: false,
-        language: index.options.force_language.clone().unwrap_or(language),
-    };
-    let file = Fossicker::new_with_data(url, data);
-    index.fossick_one(file).await
-}
-
-pub async fn write_files(index: &mut SearchState, output_path: Option<String>) {
-    index.build_indexes().await;
-
-    index.write_files(output_path.map(Into::into)).await;
-}
-
-pub async fn get_files(index: &mut SearchState) -> Vec<pagefind::SyntheticFile> {
-    index.build_indexes().await;
-    index.get_files().await
-}
+use pagefind::service::api::PagefindIndex;
 
 #[tokio::main]
 pub async fn main() {
     // read from file data.json
-    let data = std::fs::read_to_string("data.json").unwrap();
-    let json_value: serde_json::Value = serde_json::from_str(&data).unwrap();
-
-    let mut search_index = new_index(pagefind::PagefindInboundConfig {
+    let mut index = PagefindIndex::new(pagefind::PagefindInboundConfig {
         source: "source".into(),
         site: "site".into(),
         bundle_dir: None,
@@ -66,7 +19,10 @@ pub async fn main() {
         keep_index_url: false,
         service: false,
     })
-    .unwrap();
+    .expect("config is valid");
+
+    let data = std::fs::read_to_string("data.json").unwrap();
+    let json_value: serde_json::Value = serde_json::from_str(&data).unwrap();
 
     if let serde_json::Value::Array(items) = &json_value {
         for item in items {
@@ -79,20 +35,20 @@ pub async fn main() {
                 // get metadata from the object
                 let metadata = &obj["metadata"];
 
-                match add_record(
-                    &mut search_index,
-                    link.to_string(),
-                    chunk_html.to_string(),
-                    "en".to_string(),
-                    metadata.as_object().map(|m| {
-                        m.iter()
-                            .map(|(k, v)| (k.to_string(), v.to_string()))
-                            .collect()
-                    }),
-                    None,
-                    None,
-                )
-                .await
+                match index
+                    .add_record(
+                        link.to_string(),
+                        chunk_html.to_string(),
+                        "en".to_string(),
+                        metadata.as_object().map(|m| {
+                            m.iter()
+                                .map(|(k, v)| (k.to_string(), v.to_string()))
+                                .collect()
+                        }),
+                        None,
+                        None,
+                    )
+                    .await
                 {
                     Ok(_) => {
                         println!("Successfully added record");
@@ -105,12 +61,12 @@ pub async fn main() {
         }
     }
 
-    let files = get_files(&mut search_index).await;
+    let files = index.get_files().await;
     for file in files.iter() {
         // Need to write the file to the output path
-        println!("file {:?}", file.filename);
-        println!("content {:?}", file.contents);
+        println!("file {:?}", file.path);
+        println!("content {:?}", file.content);
     }
 
-    write_files(&mut search_index, Some("static/pagefind".to_string())).await;
+    index.write_files(Some("static/pagefind".to_string())).await;
 }
